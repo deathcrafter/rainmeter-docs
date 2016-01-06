35ac084f5c378c0970ed0f396e52a6d8e62880a8
Birunthan Mohanathas <birunthan@mohanathas.com>
2016-01-06 15:01:17 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,168 @@
-function hasClass(e,t){return new RegExp("(^|\\s)"+t+"(\\s|$)").test(e.className)}function addClass(e,t){hasClass(e,t)||(e.className+=(e.className?" ":"")+t)}function removeClass(e,t){hasClass(e,t)&&(e.className=e.className.replace(new RegExp("(^|\\s)*"+t+"(\\s|$)*","g"),""))}function toggleClass(e,t){hasClass(e,t)?removeClass(e,t):addClass(e,t)}!function(){"rainmeter.github.io"===location.hostname&&location.replace("http://docs.rainmeter.net")}(),function(){var e=document.getElementById("tree");if(e)for(var t,n=location.pathname.replace(/\/$/,"").replace(/^\//,""),i=e.getElementsByTagName("li"),a=0;t=i[a];++a){var s=t.firstChild;if(s){var l=t.children[1],o=s.pathname.replace(/\/$/,"").replace(/^\//,"");if(n!=o||s.hash?0!=n.indexOf(o)&&l&&addClass(l,"hide"):addClass(s,"active"),l){var d=document.createElement("span");addClass(d,"expander"),hasClass(l,"hide")&&addClass(d,"open"),d.onclick=function(e){return function(){toggleClass(e,"hide"),toggleClass(this,"open")}}(l),l.parentNode.appendChild(d)}}}}(),function(){var e=["dt","h1","h2","h3","h4","h5","h6"];for(i=0;tag=e[i];++i)for(var t,n=document.getElementsByTagName(tag),a=0;t=n[a];++a)t.id&&(t.onclick=function(e){e.ctrlKey&&(window.location.hash=this.id)})}(),function(){for(var e=document.getElementsByClassName("highlight"),t=0;div=e[t];++t){var n=document.createElement("a");n.innerHTML="Select all";var i=div.getElementsByTagName("pre")[0],a=i.offsetWidth-i.clientWidth-2;n.style.right=a+"px",a>0&&(n.style.borderRight=0),n.onclick=function(){var e=this.parentNode.firstChild;if(document.body.createTextRange){var t=document.body.createTextRange();t.moveToElementText(e),t.select()}else if(window.getSelection){var n=window.getSelection(),t=document.createRange();t.selectNodeContents(e),n.removeAllRanges(),n.addRange(t)}},div.appendChild(n);var s=div.previousElementSibling;s&&"download"==s.className&&(s.innerHTML="Download",s.style.right=a+n.offsetWidth+"px",s.style.borderRight=0,div.appendChild(s))}}(),function(){var e=document.getElementsByClassName("lightbox");if(e.length>0){var t=document.createElement("div");t.id="lightbox-wrap",t.onclick=function(){t.style.animation="",t.style.display="none"},document.body.appendChild(t);for(var n,i=0;n=e[i];++i)n.onclick=function(){var e=new Image;t.firstChild&&t.removeChild(t.firstChild),t.appendChild(e),e.onload=function(){return this.width>window.innerWidth||this.height>window.innerHeight?void(location=this.src):(e.width=this.width,e.height=this.height,e.style.marginTop=window.innerHeight/2-this.height/2+"px",t.style.display="block",t.style.outline="none",void(t.style.webkitAnimation=t.style.animation="kf-fade-in 0.4s"))},e.src=this.src}}}();
+// Helpers based on https://github.com/cferdinandi/buoy.
+function hasClass(elem, className) {
+    return new RegExp('(^|\\s)' + className + '(\\s|$)').test(elem.className);
+}
+
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+        elem.className += (elem.className ? ' ' : '') + className;
+    }
+}
+
+function removeClass(elem, className) {
+    if (hasClass(elem, className)) {
+        elem.className = elem.className.replace(new RegExp('(^|\\s)*' + className + '(\\s|$)*', 'g'), '');
+    }
+}
+
+function toggleClass(elem, className) {
+    if (hasClass(elem, className)) {
+        removeClass(elem, className);
+    } else {
+        addClass(elem, className);
+    }
+}
+
+(function() {
+	if (location.hostname === 'rainmeter.github.io') {
+		location.replace('http://docs.rainmeter.net');
+	}
+})();
+
+// Spice up the side nav.
+(function() {
+	var nav = document.getElementById('tree');
+	if (nav) {
+		var path = location.pathname.replace(/\/$/, '').replace(/^\//, '');
+		var lis = nav.getElementsByTagName('li');
+		for (var i = 0, li; li = lis[i]; ++i) {
+			var a = li.firstChild;
+			if (!a) continue;
+
+			var ul = li.children[1];
+			var aPath = a.pathname.replace(/\/$/, '').replace(/^\//, '');
+			if (path == aPath && !a.hash) {
+				addClass(a, 'active');
+			} else if (path.indexOf(aPath) != 0) {
+				if (ul) {
+					addClass(ul, 'hide');
+				}
+			}
+
+			if (ul) {
+				var span = document.createElement('span');
+				addClass(span, 'expander');
+				if (hasClass(ul, 'hide')) {
+					addClass(span, 'open');
+				}
+
+				span.onclick = (function(ul) {
+					return function() {
+						toggleClass(ul, 'hide');
+						toggleClass(this, 'open');
+					};
+				})(ul);
+
+				ul.parentNode.appendChild(span);
+			}
+		}
+	}
+})();
+
+// Make ctrl+click on <dt> and <hN> tags anchor the page with id.
+(function() {
+	var tags = ['dt', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+	for (i = 0; tag = tags[i]; ++i) {
+		var elems = document.getElementsByTagName(tag);
+		for (var j = 0, elem; elem = elems[j]; ++j) {
+			if (elem.id) {
+				elem.onclick = function(e) {
+					if (e.ctrlKey) {
+						window.location.hash = this.id;
+					}
+				};
+			}
+		}
+	}
+})();
+
+// Add 'Select all' and 'Download' buttons to <pre> blocks.
+(function() {
+	var divs = document.getElementsByClassName('highlight');
+	for (var i = 0; div = divs[i]; ++i) {
+		var selectA = document.createElement('a');
+		selectA.innerHTML = 'Select all';
+
+		var pre = div.getElementsByTagName('pre')[0];
+		var scrollWidth = pre.offsetWidth - pre.clientWidth - 2;
+		selectA.style.right = scrollWidth + 'px';
+		if (scrollWidth > 0) {
+			selectA.style.borderRight = 0;
+		}
+
+		selectA.onclick = function() {
+			var pre = this.parentNode.firstChild;
+			if (document.body.createTextRange) {
+				var range = document.body.createTextRange();
+				range.moveToElementText(pre);
+				range.select();
+			} else if (window.getSelection) {
+				var selection = window.getSelection();
+				var range = document.createRange();
+				range.selectNodeContents(pre);
+				selection.removeAllRanges();
+				selection.addRange(range);
+			}
+		};
+
+		div.appendChild(selectA);
+
+		var downloadA = div.previousElementSibling;
+		if (downloadA && downloadA.className == 'download') {
+			downloadA.innerHTML = 'Download';
+			downloadA.style.right = scrollWidth + selectA.offsetWidth + 'px';
+			downloadA.style.borderRight = 0;
+			div.appendChild(downloadA);
+		}
+	}
+})();
+
+(function() {
+	var elems = document.getElementsByClassName('lightbox');
+	if (elems.length > 0) {
+		var wrap = document.createElement('div');
+		wrap.id = 'lightbox-wrap';
+		wrap.onclick = function() {
+			wrap.style.animation = '';
+			wrap.style.display = 'none';
+		};
+
+		document.body.appendChild(wrap);
+
+		for (var i = 0, elem; elem = elems[i]; ++i) {
+			elem.onclick = function() {
+				var img = new Image();
+				if (wrap.firstChild) wrap.removeChild(wrap.firstChild);
+				wrap.appendChild(img);
+
+				img.onload = function() {
+					// Switch to the image if it's too large.
+					if (this.width > window.innerWidth || this.height > window.innerHeight) {
+						location = this.src;
+						return;
+					}
+
+					// Center image.
+					img.width = this.width;
+					img.height = this.height;
+					img.style.marginTop = ((window.innerHeight / 2) - (this.height / 2)) + 'px';
+
+					wrap.style.display = 'block';
+					wrap.style.outline = 'none';
+					wrap.style.webkitAnimation = wrap.style.animation = 'kf-fade-in 0.4s';
+				};
+				img.src = this.src;
+			};
+		}
+	}
+})();
