a5e939afde5f0d4e7bfb2a8286481a88e951052f
brianferguson <ferguson.brian@gmail.com>
2017-10-15 02:57:18 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -79,10 +79,20 @@ function toggleClass(elem, className) {
 		var selectA = document.createElement('a');
 		selectA.innerHTML = 'Select all';
 
+		// Force the 'Select ALl' button to use the same color as 'Download' button via bootstrap
+		selectA.href= 'javascript:void(0);';
+
 		var pre = div.getElementsByTagName('pre')[0];
 		var scrollWidth = pre.offsetWidth - pre.clientWidth;
 
-		selectA.style.right = scrollWidth + 1 + 'px';
+		selectA.style.right = scrollWidth + 'px';
+		if (scrollWidth > 2) {
+			selectA.style.borderRight = 0;
+		}
+		else {
+			selectA.style.borderRight = '.05rem';
+			selectA.style.borderTopRightRadius = '.25rem';
+		}
 
 		selectA.style.top += 1 + 'px';
 
